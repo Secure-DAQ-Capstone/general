29fec52c6123c147213b22ac8a1029ab3dd92d27
gpearcey <pearcey.grace@gmail.com>
2025-02-05 14:37:53 -0330

    print packet

--- main.cpp
+++ main.cpp
@@ -18,7 +18,8 @@
 #include <google/protobuf/util/time_util.h>
 //#include <N2kMessagesEnumToStr.h>
 using namespace std;
-
+using google::protobuf::util::TimeUtil;
+using namespace tutorial;
 typedef struct {
   unsigned long PGN;
   void (*Handler)(const tN2kMsg &N2kMsg); 
@@ -30,6 +31,7 @@ void Temperature(const tN2kMsg &N2kMsg);
 void OutsideEnvironmental(const tN2kMsg &N2kMsg);
 void Humidity(const tN2kMsg &N2kMsg);
 void TemperatureExt(const tN2kMsg &N2kMsg);
+void printPacket(const Packet& packet);
 
 template<typename T> void PrintLabelValWithConversionCheckUnDef(const char* label, T val, double (*ConvFunc)(double val)=0, bool AddLf=false, int8_t Desim=-1 ) {
   serStream.print(label);
@@ -93,6 +95,26 @@ void Temperature(const tN2kMsg &N2kMsg) {
 
       packet.set_allocated_data(data);
 
+      packet.set_board_id(123);
+      
+      // Set timestamp
+      *packet.mutable_time_data_read() = TimeUtil::GetCurrentTime();
+
+
+
+      // Set protocol
+      packet.set_protocol(tutorial::Packet::CAN);
+
+      // Set original message
+      packet.set_original_message("\xDE\xAD\xBE\xEF", 4);
+
+      // Set digital signature
+      packet.set_digital_signature(9876543210);
+
+      // Print the packet
+      printPacket(packet);
+
+
     } else {
       serStream.print("Failed to parse PGN: ");  serStream.println(N2kMsg.PGN);
     }
@@ -143,6 +165,46 @@ void HandleNMEA2000Msg(const tN2kMsg &N2kMsg) {
     NMEA2000Handlers[iHandler].Handler(N2kMsg); 
   }
 }
+
+// Function to print the Packet message in a readable format
+void printPacket(const Packet& packet) {
+    cout << "Packet Message:" << endl;
+    cout << "  Board ID: " << packet.board_id() << endl;
+
+    // Convert timestamp to a human-readable string
+    cout << "  Time Data Read: " << TimeUtil::ToString(packet.time_data_read()) << endl;
+
+    // Print Data fields
+    if (packet.has_data()) {
+        cout << "  Data:" << endl;
+        cout << "    Type: " << packet.data().type() << endl;
+        cout << "    Fields:" << endl;
+        for (const auto& field : packet.data().fields()) {
+            cout << "      - " << field.label() << ": " << field.value() << endl;
+        }
+    }
+
+    // Print protocol
+    cout << "  Protocol: ";
+    switch (packet.protocol()) {
+        case Packet::CAN: cout << "CAN"; break;
+        case Packet::Modbus: cout << "Modbus"; break;
+        default: cout << "Unknown";
+    }
+    cout << endl;
+
+    // Print original message as hex
+    cout << "  Original Message: ";
+    for (unsigned char c : packet.original_message()) {
+        printf("%02X ", c);
+    }
+    cout << endl;
+
+    // Print digital signature
+    cout << "  Digital Signature: " << packet.digital_signature() << endl;
+}
+
+
 
 int main(void)
 {
