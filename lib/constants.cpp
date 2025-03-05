12dc901f7e996cfae43f85fa801b51371219d4f2
Matthew Grouchy <mattgrouchy@gmail.com>
2025-03-05 15:16:58 -0330

    Add david's board id to the constants

--- lib/constants.cpp
+++ lib/constants.cpp
@@ -9,6 +9,7 @@ const std::vector<MachineInfo> MachineRegistry::machines = {
     {2, "Ventana 2", "4088253719cb401487ea11178840430d"},
     {3, "Ventana 3", "4088253719cb401487ea11178840430d3"},
     {4, "Ventana 4", "4088253719cb401487ea11178840430d4"},
+    {6, "David", "6e824f19507d4a1dac5e15101c0ae908"},
     // Add other machines as needed
 };
 
